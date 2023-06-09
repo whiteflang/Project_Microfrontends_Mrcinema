3d3f99a4805a9de0d8898fbd6b2e8962d299adee
Sebastian Rivas <jsebas400@hotmail.com>
2023-06-09 11:29:17 -0500

    App logo updated

--- container/src/app/app.component.ts
+++ container/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <ul>
-  <li><img src="../assets/angular.png" width="50"></li>
+  <li><img src="../assets/logo.ico" width="50"></li>
   <li><a routerLink="/">Home</a></li>
   <li><a routerLink="/movies/list">Movies</a></li>
  </ul>
