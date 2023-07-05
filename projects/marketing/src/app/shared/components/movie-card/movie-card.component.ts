c7e26b0055e46780d85236cc55402affc347c79e
Fabián Hernández Nieto <fabian.nieto@xpo.com>
2023-07-05 18:45:53 -0500

    add missing components of marketing project

---
+++ projects/marketing/src/app/shared/components/movie-card/movie-card.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: 'movie-card.component.html',
+  styleUrls: ['movie-card.component.css'],
+})
+export class MovieCardComponent {}
