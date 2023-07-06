4808c56eebcdea4bbfea73f13c2eb460143d2c01
whiteflang <92933674+whiteflang@users.noreply.github.com>
2023-07-06 17:31:37 -0500

    fixed of styles and template home

--- projects/container/src/app/app.component.ts
+++ projects/container/src/app/app.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-header></app-header>
-
-    <app-sliders>/</app-sliders>
-
-    <router-outlet></router-outlet>
-
-    <app-footer></app-footer>
-  `,
+  templateUrl: './app.component.html',
   styles: []
 })
 export class AppComponent {
--- projects/container/src/app/home/home.component.ts
+++ projects/container/src/app/home/home.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
+  template:` 
+  <app-header></app-header>
+  <app-sliders>/</app-sliders>
+  <div style="background:white; whidth:100vh; height:80vh; " ></div>
+  <app-footer></app-footer>
   `,
-  styles: [
-  ]
+  styles:[]
 })
 export class HomeComponent {
 
