294cfaa11e2684a24942cf4eb37e2714687f9010
snaphuman <fhernandezn@gmail.com>
2023-07-06 20:54:03 -0500

    integrate featuredMovies component in container plus fix navigation issues

--- projects/container/src/app/home/home.component.ts
+++ projects/container/src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild('featuredMovies', {read: ViewContainerRef}) featuredMoviesContainer!: ViewContainerRef;
 
+
+  ngOnInit(): void {
+
+    this.loadFeaturedMovies();
+  }
+
+  async loadFeaturedMovies() {
+    const m = await loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'marketing',
+      exposedModule: './FeaturedMoviesComponent'
+    });
+
+    const ref = this.featuredMoviesContainer.createComponent(m.FeaturedMoviesComponent);
+  }
 }
