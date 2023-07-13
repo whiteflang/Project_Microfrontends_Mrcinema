84d066211c1d300d10e8f1e4cf52dd23058c97c8
s2023 <jsebas400@hotmail.com>
2023-07-13 10:34:55 -0500

    List movies styles mockup implemented

--- projects/marketing/src/app/shared/components/movie-card/movie-card.component.ts
+++ projects/marketing/src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-movie-card',
+  selector: 'shared-movie-card',
   templateUrl: 'movie-card.component.html',
   styleUrls: ['movie-card.component.css'],
 })
