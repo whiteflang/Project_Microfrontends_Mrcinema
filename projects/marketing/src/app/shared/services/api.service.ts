80083fe8e5c336d0e2b01d5c0627a6dbee9eadc2
Fabián Hernández <fhernandezn@gmail.com>
2023-07-25 13:46:48 +0000

    NEEDS REVIEW: add headers configuration for angular request

--- projects/marketing/src/app/shared/services/api.service.ts
+++ projects/marketing/src/app/shared/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../interfaces/api.interface';
 
@@ -8,15 +8,18 @@ export class APIService {
 
   private nowPlayingUrl = 'http://api.test.mrcinema.yuju/cinema/v1/movie/nowPlaying';
   private premiereUrl = 'http://api.test.mrcinema.yuju/cinema/v1/movie/premiere';
+  headers: HttpHeaders;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {
+	const headers = new HttpHeaders();
+	this.headers = headers.set('Access-Control-Allow-Origin','*');
+  }
 
   getNowPlayingMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.nowPlayingUrl);
+    return this.http.get<Movie[]>(this.nowPlayingUrl, {headers: this.headers});
   }
 
   getPremiereMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.premiereUrl);
+    return this.http.get<Movie[]>(this.premiereUrl, {headers: this.headers});
   }
-
 }
