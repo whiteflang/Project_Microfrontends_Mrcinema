17df151a8bd4ddd87374599fc475859539a61370
Leviathan9020 <jomar2030@hotmail.com>
2023-08-02 09:50:01 -0500

    Translate new changes

--- projects/backoffice/src/components/header/Header.jsx
+++ projects/backoffice/src/components/header/Header.jsx
@@ -5,8 +5,7 @@ const Header = () => {
   return (
     <div>
       <header className="header-container">       
-        <h2>Movies</h2>
-        <h2>Admin</h2>
+        <h2>Movies</h2>        
       </header>
     </div>
   );
--- projects/backoffice/src/components/header/Sidebar.jsx
+++ projects/backoffice/src/components/header/Sidebar.jsx
@@ -17,4 +17,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
